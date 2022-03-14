bdc96d67f4f3dda79c2d92ffdf179e1999686460
gus <19171@wc.school.nz>
2022-03-15 11:54:45 +1300

    Added comments to most recent code (possibly more to come)

--- Game/testgame.py
+++ Game/testgame.py
@@ -8,7 +8,7 @@ y = 400
 width = 64
 height = 64
 
-
+#---------------------------- Variables and lists ----------------------------
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'),
              pygame.image.load('R3.png'), pygame.image.load('R4.png'),
              pygame.image.load('R5.png'), pygame.image.load('R6.png'),
@@ -24,39 +24,48 @@ char = pygame.image.load('standing.png')
 clock = pygame.time.Clock()
 
 class player(object):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height): # Pass through Variables
         self.x=x
         self.y=y
         self.width=width
         self.height=height
-        self.velocity = 5
-        self.run = True
-        self.is_jump = False
+        self.velocity = 5    # These are no longer global variable, they are now class variables.
+        self.run = True      # This is better because they can only be accessed by the player class
+        self.is_jump = False # and not other parts of the program.
         self.jump_count = 10
         self.left = False
         self.right = False
         self.walkCount = 0
     def draw(self, window):
-        if self.walkCount + 1 >= 27:
+        # We have 9 images for our walking animation, I want to show the same image for 3 frames
+        # so i use the number 27 as an upper bound for walkCount because 27 / 3 = 9. So 9 images shown
+        # 3 times each animation
+        if self.walkCount + 1 >= 27: # Because each direction is made up of 9 images shown 3 times this will
+                                    # stop a index out of range error with our list as it only has 9 values.
             self.walkCount = 0
         if self.left:
-            window.blit(walkLeft[self.walkCount//3], (self.x,self.y))
-
-
-
+            window.blit(walkLeft[self.walkCount//3], (self.x,self.y)) # We integer divide (Rounding down to a even
+                                                                      # number) WalkCount by 3 to ensure each image
+                                                                      # is shown 3 times every animation. (x,y)will
+                                                                      # keep track of the curent position of the
+                                                                      # character based on the user input .
             self.walkCount += 1
         elif self.right:
             window.blit(walkRight[self.walkCount//3], (self.x,self.y))
             self.walkCount += 1
         else:
-            window.blit(char, (self.x, self.y))
+            window.blit(char, (self.x, self.y)) # If the character is standing still.
+
+#---------------------------- Methods ----------------------------
 def redraw_GameWindow():
-    window.blit(bg, (0,0))
+    window.blit(bg, (0,0)) # This will draw our background image at (0,0) .blit is short for block
+    # transfer and allows us to add images to a surface, in this case the main window.
     player_character.draw(window)
     pygame.display.update()
 
+#---------------------------- Main Routine ----------------------------
 run=True
-player_character=player(300,410,64,64)
+player_character=player(300,410,64,64) # Object created from the class which sends information to the pass through variables
 while run:
     clock.tick(27)
     for event in pygame.event.get():
@@ -76,17 +85,17 @@ while run:
         player_character.left = False
         player_character.right = False
     if not (player_character.is_jump):
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE]: # Jumping
             player_character.is_jump = True
-            player_character.right = False
-            player_character.left = False
-            player_character.walkCount = 0
+            player_character.right = False # Stops animation while jumping
+            player_character.left = False # Stops animation while jumping
+            player_character.walkCount = 0 # Stops animation while jumping
     else:
         if player_character.jump_count >= -10:
-            negitive = 1
-            if player_character.jump_count < 0:
-                negitive = -1
-            player_character.y -= (player_character.jump_count ** 2) * 0.5 * negitive
+            negitive = 1 # New Code
+            if player_character.jump_count < 0: # New Code
+                negitive = -1 # code
+            player_character.y -= (player_character.jump_count ** 2) * 0.5 * negitive # New Code
             player_character.jump_count -= 1
         else:
             player_character.is_jump = False
