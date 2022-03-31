61d1f4b8ddde5746e6dd0f76f36217b9c352b9d7
gus <19171@wc.school.nz>
2022-03-31 15:07:26 +1300

    Added new death screen background, alongside visibilty of final score upon death. Removed sprite init on player and changed it back to object, made the hitbox blend in, and added comments all around the code

--- sledRacing/sledracing.py
+++ sledRacing/sledracing.py
@@ -1,10 +1,12 @@
+# Imports and setups
 import pygame
 from random import randint
 from pygame import time
 from time import sleep
 pygame.init()
-window = pygame.display.set_mode((1100, 650))
-pygame.display.set_caption("Danger Sledding")
+window = pygame.display.set_mode((1100, 650)) # Sets window size to 1100 x 650
+pygame.display.set_caption("Danger Sledding") # Sets window name
+# Variables And Lists
 Points = 0
 x = 500
 y = 100
@@ -13,21 +15,15 @@ height = 64
 enemyY = 600
 enemyx = 500
 rise_velocity = 5
-#-- Variables and lists --
 bg = pygame.image.load('bg.png')
 char = pygame.image.load('racer.png')
 bad = pygame.image.load('rock.png')
+title = pygame.image.load('title.png')
 clock = pygame.time.Clock()
 
-
-class AbstractGroup:
-    pass
-
-
-class player(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, *groups: AbstractGroup):
-        super().__init__(*groups)
-        pygame.sprite.Sprite.__init__(self)
+#Classes
+class player(object):
+    def __init__(self, x, y, width, height):
         self.x=x
         self.y=y
         self.width=width
@@ -38,14 +34,11 @@ class player(pygame.sprite.Sprite):
         self.right = False
         self.walkCount = 0
         self.hitbox = (self.x + 27, self.y + 40, 39, 50)
-        self.rect = pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
+        self.rect = pygame.draw.rect(window, (255, 255, 255), self.hitbox, 2)
     def draw(self, window):
         window.blit(char, (self.x, self.y))
-        pygame.sprite.Sprite.__init__(self)
         self.hitbox = (self.x + 27, self.y + 40, 39, 50)
-        self.rect = pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
-    def collision(self):
-        print("Collision")
+        self.rect = pygame.draw.rect(window, (255, 255, 255), self.hitbox, 2)
 
 
 class enemy(object):
@@ -56,11 +49,11 @@ class enemy(object):
         self.height=height
         self.rise=rise_velocity
         self.hitbox = (self.x + 30, self.y + 50, 48, 35)
-        self.rect = pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
+        self.rect = pygame.draw.rect(window, (255, 255, 255), self.hitbox, 2)
     def draw(self, window):
         window.blit(bad, (self.x, self.y))
         self.hitbox = (self.x + 28, self.y + 50, 48, 35)
-        self.rect = pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
+        self.rect = pygame.draw.rect(window, (255, 255, 255), self.hitbox, 2)
 def redraw_GameWindow():
     window.blit(bg, (0, 0))
     player_character.draw(window)
@@ -70,16 +63,22 @@ def redraw_GameWindow():
     pygame.display.update()
 
 def redrawEndgame_window():
-    window.blit(bg, (0, 0))
+    window.blit(title, (0, 0))
+    text = font.render("You scored " + str(Points) + " Points!", 1, (0, 0, 0))
+    text2 = font2.render("You Died!", 1, (0, 0, 0))
+    window.blit(text, (400, 350))
+    window.blit(text2, (400, 200))
     pygame.display.update()
 
+# Main game
 score = 0
 run = True
 alive = True
-font=pygame.font.SysFont("comicsansms", 30, True, True)
-player_character=player(500, 100, 64, 64)
+font=pygame.font.SysFont("comicsansms", 30, True, True) # Setsup fonts for the score and end game screen.
+font2=pygame.font.SysFont("comicsansms", 60, True, True)
+player_character=player(500, 100, 64, 64) # Setting passthrough variables for the player and the enemies
 enemy_object=enemy(500, 600, 64, 64)
-while run:
+while run: # This is the game loop
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -99,16 +98,15 @@ while run:
         elif keys[pygame.K_RIGHT] and player_character.x < 930 - player_character.velocity - player_character.width:
             player_character.x += player_character.velocity
         enemy_object.y -= enemy_object.rise
-        if keys[pygame.K_ESCAPE]:
+        if keys[pygame.K_ESCAPE]: # Closes the game on Escape Button Press
             run = False
             alive = False
-        if player_character.rect.colliderect(enemy_object.rect):
-            print("Collision")
-            print(Points)
-            player_character.kill()
+        if player_character.rect.colliderect(enemy_object.rect): # Upon collision, goes to end game screen and stops
+                                                                 # The main game loop of 'alive'
             alive = False
-        if enemy_object.y == 0:
-            Points += 1
+        if enemy_object.y == 0: # Resets the Y value of the enemy and places it at a random x value
+                                # To have a constant flow of enemies one after the other
+            Points += 1   # This adds a point every time the enemy is successfully avoided by the player
             enemy_object.y = 600
             enemy_object.x = randint(330, 930)
         redraw_GameWindow()
